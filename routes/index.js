632fa3403609514a7781535a9009d56f95feb687
benjdj6 <benjdj6@vt.edu>
2017-10-23 20:27:04 -0700

    Added aqi/:zip stub

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'AirNow Alert' });
 });
 
+router.get('/aqi/:zip', function(req, res, next) {
+  res.sendStatus(204);
+});
+
 module.exports = router;
