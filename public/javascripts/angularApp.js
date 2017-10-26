fcd904845ea798683657a72b3bcffa1637f42a63
benjdj6 <benjdj6@vt.edu>
2017-10-25 21:45:20 -0700

    Added getAQI function to angularApp

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,8 +1,21 @@
 let app = angular.module('AirNow-Alert', []);
 
-app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-    //temp empty
+app.controller('MainCtrl', [
+  '$scope', 
+  '$http',
+  function($scope, $http){
+
+    $scope.getAQI = function() {
+      $scope.AQI = -1;
+      return $http({
+        method: 'GET',
+        url: '/aqi/' + $scope.zip
+      }).success(function(data) {
+        if(data == 404) {
+          return alert("No Results :(");
+        }
+        console.log(data);
+      });
+    };
+    
 }]);
