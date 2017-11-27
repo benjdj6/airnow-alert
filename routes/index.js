b872e37e620c994dcb107ae1a10c3a58822579e1
benjdj6 <benjdj6@vt.edu>
2017-11-26 19:26:20 -0800

    Added twilio require to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var request = require('request');
 
 var router = express.Router();
 
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+
+const client = require('twilio')(accountSid, authToken);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'AirNow Alert' });
