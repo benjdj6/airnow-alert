287c042816714805ebfb7f2ae224b0664346dee9
benjdj6 <benjdj6@vt.edu>
2017-12-24 22:23:30 -0500

    Added mongoose and passport imports

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var passport = require('passport');
 
 var app = express();
 
