81c3f31d2f1b5dd7b2f093a203c22cbce06792ff
benjdj6 <benjdj6@vt.edu>
2018-01-06 22:28:09 -0800

    Added necessary dependencies to users route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var passport = require('passport');
+
+var jwt = require('express-jwt');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
