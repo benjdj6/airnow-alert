52d8eb25f3a1549e59e9791e601ea2a6c84b1837
benjdj6 <benjdj6@vt.edu>
2018-01-07 22:08:01 -0800

    Added registration endpoint

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,25 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// POST new user
+router.post('/register', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+    
+    return res.json({ token: user.generateJWT() });
+  });
+});
+
 module.exports = router;
