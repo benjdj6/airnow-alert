c1412f110689fe61b516c417bfe4957bd6a0b50c
benjdj6 <benjdj6@vt.edu>
2018-01-09 21:05:18 -0800

    Added login endpoint

--- routes/index.js
+++ routes/index.js
@@ -34,4 +34,23 @@ router.get('/aqi/:zip', function(req, res, next) {
    });
 });
 
+// POST new user session/login
+router.post('/login', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields' });
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      returnnext(err);
+    }
+
+    if (user) {
+      return res.json({ token: user.generateJWT() });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
 module.exports = router;
